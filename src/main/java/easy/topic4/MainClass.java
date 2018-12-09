aa167d669bea90299341be603b8dacf88ca873eb
Dancin <752827339@qq.com>
2018-12-09 20:15:27 +0800

    commit easy topic4 and it's solution

---
+++ src/main/java/easy/topic4/MainClass.java
@@ -0,0 +1,54 @@
+package easy.topic4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
+
+public class MainClass {
+    public static String stringToString(String input) {
+        if (input == null) {
+            return "null";
+        }
+        return input.trim();
+//        return Json.value(input).toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+
+            int ret = new Solution().romanToInt(s);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
